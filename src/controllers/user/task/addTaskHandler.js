555e9d075afeb5671b42feb06be00beda64c14ab
Alaa Alser <alaalsser@gmail.com>
2020-12-16 18:56:59 +0200

    addtsake

--- src/controllers/user/task/addTaskHandler.js
+++ src/controllers/user/task/addTaskHandler.js
@@ -3,3 +3,28 @@ const addTaskHandler = (req, res, next) => {
 };
 
 module.exports = { addTaskHandler };
+
+const { addTask } = require('../../../database/queries');
+const { addListSchema } = require('../../../utils/validation');
+const { boomify } = require('../../../utils/boomify');
+
+const addListHandler = (req, res, next) => {
+  try {
+    const { taskDetail, todoListId } = req.body;
+    const { userId } = req;
+
+    addTask(todoListId, taskDetail)
+      .then((data) => data.rows[0])
+      .then((data) => {
+        res.json({
+          msg: 'new task created',
+          data,
+        });
+      })
+      .catch((err) => next(boomify(400, err.message)));
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { addListHandler };
--- src/routes/task.js
+++ src/routes/task.js
@@ -1,3 +1,7 @@
 const router = require('express').Router();
+const { addTaskHandler } = require('../controllers');
+const { authenticate } = require('../middleware/authentication');
+
+router.post('/', authenticate, addTaskHandler);
 
 module.exports = router;
