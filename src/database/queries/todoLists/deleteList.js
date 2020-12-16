337556c79594051ef35063847c7a42c319624caa
Alaa Alser <alaalsser@gmail.com>
2020-12-16 16:50:55 +0200

    delete router Relates #12

--- src/database/queries/todoLists/deleteList.js
+++ src/database/queries/todoLists/deleteList.js
@@ -2,7 +2,7 @@ const connection = require('../../config/connection');
 
 const deleteList = (userId, todoListID) => {
   const sql = {
-    text: 'DELETE FROM todoLists WHERE userId =$1 AND id =$2',
+    text: 'DELETE FROM todoLists WHERE userId =$1 AND id =$2 RETURNING *',
     values: [userId, todoListID],
   };
   return connection.query(sql);
