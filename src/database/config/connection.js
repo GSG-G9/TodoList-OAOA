46faeff85fbc6fc5ad4b4c97283992450b8b4908
Osama-you <osama.w.younis@gmail.com>
2020-12-16 08:25:13 +0200

    make the sql query Relates #24

--- src/database/config/connection.js
+++ src/database/config/connection.js
@@ -7,10 +7,10 @@ switch (process.env.NODE_ENV) {
     DB_URL = process.env.DATABASE_URL;
     break;
   case 'development':
-    DB_URL = process.env.DB_URL_ENV;
+    DB_URL = process.env.DB_URL_DEV;
     break;
   case 'test':
-    DB_URL = process.env.DB_URL_ENV;
+    DB_URL = process.env.DB_URL_TEST;
     break;
   default:
     throw new Error('there is no database url');
--- src/database/queries/getUserData.js
+++ src/database/queries/getUserData.js
@@ -1,9 +1,45 @@
 const connection = require('../config/connection');
 
-const getUserData = () => {
-  const sql = {
+const getUserData = (userId) => {
+  const userData = {};
+  let sql = {
+    text: 'SELECT id, firstname, lastname, email from users where id = $1;',
+    values: [userId],
   };
-  return connection.query(sql);
+
+  return connection.query(sql).than((data) => data.rows[0])
+    .than((userInfo) => {
+      userData.userInfo = userInfo;
+      // new sql query get all task data
+      sql = {
+        text: `SELECT tasks.id AS taskId, tasks.details, tasks.status, todolists.id AS todoListID, todolists.title FROM tasks INNER JOIN todolists
+      ON tasks.todolistid = todoListID WHERE todolists.userid = 1;`,
+        values: [userId],
+      };
+      return connection.query(sql);
+    }).then((data) => data.rows)
+    .then((data) => {
+      // filtering the data
+      data.forEach((item) => {
+        const {
+          todolistId, taskId, details, status, title,
+        } = item;
+
+        let todolistidOnUser = userData.userLists[todolistId];
+        if (!todolistidOnUser) {
+          todolistidOnUser = {
+            title,
+            tasks: [],
+          };
+        }
+        todolistidOnUser.tasks.push({
+          taskId,
+          details,
+          status,
+        });
+      });
+      return userData;
+    });
 };
 
 module.exports = { getUserData };
