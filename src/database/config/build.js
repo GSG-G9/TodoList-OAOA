1ee89a68be1b692916a6e7af759193acc81a7f27
Osama-you <osama.w.younis@gmail.com>
2020-12-16 07:01:40 +0200

    Convert callback JWT to promise Relates #14

--- src/database/config/build.js
+++ src/database/config/build.js
@@ -1,15 +1,14 @@
+/* eslint-disable no-console */
 const { join } = require('path');
 const { readFileSync } = require('fs');
 const connection = require('./connection');
 
-function buildDB = (name) =>  {
-
+const buildDB = (name) => {
   const sql = readFileSync(join(__dirname, `${name}.sql`)).toString();
   connection
     .query(sql)
-    .then(() => console.log('DB created successfuly'))
-    .catch(err => console.log(err));
-
-}
+    .then(() => console.log('DB created successfully'))
+    .catch((err) => console.log(err));
+};
 
 module.exports = buildDB;
