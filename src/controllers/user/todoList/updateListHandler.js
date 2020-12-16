31ec530375e667e4473fe3c3c310967d95dbda80
AdhamHaisami <adham9633@gmail.com>
2020-12-16 16:42:03 +0200

    start working

--- src/controllers/user/todoList/updateListHandler.js
+++ src/controllers/user/todoList/updateListHandler.js
@@ -1,5 +1,10 @@
+const { verify } = require('../../../utils/validation');
+
+process.env.SECRET_TOKEN;
 const updateListHandler = (req, res, next) => {
 
 };
 
 module.exports = { updateListHandler };
+
+// verify token => check token id DB => list id => newName
